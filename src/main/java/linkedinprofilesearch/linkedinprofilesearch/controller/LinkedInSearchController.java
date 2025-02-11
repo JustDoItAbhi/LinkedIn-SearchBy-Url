9273534b0fd3b6073b163e69a518988014c36a9a
Admin <erarvinder2012@gmail.com>
2025-02-11 02:14:22 +0200

    updated<UPDATED>

--- src/main/java/linkedinprofilesearch/linkedinprofilesearch/controller/LinkedInSearchController.java
+++ src/main/java/linkedinprofilesearch/linkedinprofilesearch/controller/LinkedInSearchController.java
@@ -15,9 +15,8 @@ import java.util.List;
 @RestController
 @RequestMapping("/linkedin")
 public class LinkedInSearchController {
-    @Autowired
     private ISearchService searchService;
-
+    @Autowired
     public LinkedInSearchController(ISearchService searchService) {
         this.searchService = searchService;
     }
